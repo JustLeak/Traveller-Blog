a6b0d778af12bfb9af1409ba27edda077b657d20
JustLeak <palex4ee@gmail.com>
2018-11-04 12:10:55 +0300

    UserNotesActivity is created.

--- app/src/main/java/com/android/evgeniy/firebaseblog/activities/UserNotesActivity.java
+++ app/src/main/java/com/android/evgeniy/firebaseblog/activities/UserNotesActivity.java
@@ -1,15 +1,64 @@
 package com.android.evgeniy.firebaseblog.activities;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.android.evgeniy.firebaseblog.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.GenericTypeIndicator;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.List;
 
 public class UserNotesActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    DatabaseReference myRef;
+
+    private List<String> noteTextList;
+
+    private ListView noteListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_notes);
+
+        noteListView = (ListView) findViewById(R.id.note_text);
+
+        myRef = FirebaseDatabase.getInstance().getReference();
+
+        FirebaseUser user = mAuth.getInstance().getCurrentUser();
+
+        myRef.child(user.getUid()).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                GenericTypeIndicator<List<String>> t = new GenericTypeIndicator<List<String>>() {
+                };
+                noteTextList = dataSnapshot.child("User notes").getValue(t);
+
+                updateUI();
+            }
+
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    private void updateUI() {
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(getBaseContext(), android.R.layout.simple_list_item_1, noteTextList);
+
+        noteListView.setAdapter(adapter);
     }
 }
