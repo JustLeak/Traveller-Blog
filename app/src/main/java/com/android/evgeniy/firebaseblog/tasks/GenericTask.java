af334622039f8adad6ed6074e89fb1e5a0852e68
matyc777 <matyc777@gmail.com>
2018-12-14 18:09:53 +0300

    Generic Task added

---
+++ app/src/main/java/com/android/evgeniy/firebaseblog/tasks/GenericTask.java
@@ -0,0 +1,63 @@
+package com.android.evgeniy.firebaseblog.tasks;
+
+import android.os.AsyncTask;
+import android.support.v7.widget.RecyclerView;
+
+import com.android.evgeniy.firebaseblog.adapters.FriendsRecyclerAdapter;
+import com.android.evgeniy.firebaseblog.adapters.NotesRecyclerAdapter;
+import com.android.evgeniy.firebaseblog.models.Friend;
+import com.android.evgeniy.firebaseblog.models.UserNote;
+import com.google.firebase.database.DataSnapshot;
+
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class GenericTask<T> extends AsyncTask<DataSnapshot, Integer, ArrayList<T>> {
+    private RecyclerView.Adapter adapter;
+    private Class<T> genericType;
+
+    public GenericTask(RecyclerView.Adapter adapter, Class<T> type) {
+        this.adapter = adapter;
+        genericType = type;
+    }
+
+    @Override
+    protected ArrayList<T> doInBackground(DataSnapshot... snapshots) {
+        ArrayList<T> friends = new ArrayList<>();
+        for (DataSnapshot dataS : snapshots[0].getChildren()) {
+            friends.add(dataS.getValue(genericType));
+        }
+        Collections.reverse(friends);
+
+        return friends;
+    }
+
+    @Override
+    protected void onPostExecute(ArrayList<T> resultList) {
+        super.onPostExecute(resultList);
+
+        if (genericType.equals(Friend.class)) {
+            FriendsRecyclerAdapter friendsRecyclerAdapter = (FriendsRecyclerAdapter) adapter;
+            friendsRecyclerAdapter.update((ArrayList<Friend>) resultList);
+        } else if (genericType.equals(UserNote.class)) {
+            NotesRecyclerAdapter notesRecyclerAdapter = (NotesRecyclerAdapter) adapter;
+            notesRecyclerAdapter.update((ArrayList<UserNote>) resultList);
+        }
+    }
+
+    private Class<T> getGenericClass() {
+        Class<T> type = null;
+        Type mySuperclass = getClass().getGenericSuperclass();
+        Type tType = ((ParameterizedType) mySuperclass).getActualTypeArguments()[0];
+        String className = tType.toString().split(" ")[1];
+        try {
+            type = (Class<T>) Class.forName(className);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        return type;
+    }
+}
