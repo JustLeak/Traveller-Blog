dc793546b08012eff595691b166e3bf505d6749e
matyc777 <matyc777@gmail.com>
2018-12-16 16:18:15 +0300

    resize note icon

--- app/src/main/java/com/android/evgeniy/firebaseblog/services/BitmapCreator.java
+++ app/src/main/java/com/android/evgeniy/firebaseblog/services/BitmapCreator.java
@@ -9,9 +9,9 @@ import com.google.android.gms.maps.model.MarkerOptions;
 public final class BitmapCreator {
     public static Bitmap getBitmap(Drawable drawable) {
         Canvas canvas = new Canvas();
-        Bitmap bitmap = Bitmap.createBitmap(drawable.getIntrinsicWidth(), drawable.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
+        Bitmap bitmap = Bitmap.createBitmap((int) (drawable.getIntrinsicWidth() * 1.2), (int) (drawable.getIntrinsicHeight() * 1.2), Bitmap.Config.ARGB_8888);
         canvas.setBitmap(bitmap);
-        drawable.setBounds(0, 0, drawable.getIntrinsicWidth(), drawable.getIntrinsicHeight());
+        drawable.setBounds(0, 0, (int) (drawable.getIntrinsicWidth() * 1.2), (int) (drawable.getIntrinsicHeight() * 1.2));
         drawable.draw(canvas);
 
         return bitmap;
